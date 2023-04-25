777813860930f80aa16444d49411f529eaf6ea41
Sunny-25 <potturipraveen7@gmail.com>
2023-04-25 17:27:42 +0530

    frontend added

---
+++ frontend/components/Header.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+	return (
+		<header>
+			<Link href={"/"}>Ecommerce</Link>
+			<nav></nav>
+		</header>
+	);
+};
+
+export default Header;
---
+++ frontend/pages/index.js
@@ -0,0 +1,13 @@
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Header from "@/components/Header";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+	return (
+		<div>
+			<Header />
+		</div>
+	);
+}
